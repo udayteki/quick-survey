20cf7d7ba1a89f95e772fa79f725832f0d63f9b3
Simon <svansintjan@gmail.com>
2016-10-06 17:22:49 -0800

    [WIP] Start adding e-mail. Todo with #41.

--- server/startup/setupSite.js
+++ server/startup/setupSite.js
@@ -1,3 +1,18 @@
+// TODO: Implement capnp and networking.
+// Capnp = require('capnp');
+// var SandstormHttpBridge = Capnp.importSystem("sandstorm/sandstorm-http-bridge.capnp").SandstormHttpBridge;
+
+// var capnpConnection = null;
+// var httpBridge = null;
+
+// function getHttpBridge() {
+//   if (!httpBridge) {
+//     capnpConnection = Capnp.connect("unix:/tmp/sandstorm-api");
+//     httpBridge = capnpConnection.restore(null, SandstormHttpBridge);
+//   }
+//   return httpBridge;
+// }
+
 Meteor.startup(function () {
   // Run set up things here.
 });

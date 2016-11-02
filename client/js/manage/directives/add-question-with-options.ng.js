4961200b2175a599e530061cd457cdc6b948c19c
Simon <svansintjan@gmail.com>
2016-11-02 12:08:36 -0800

    Remove ability to add multiple 'Other'. Close #39

--- client/js/manage/directives/add-question-with-options.ng.js
+++ client/js/manage/directives/add-question-with-options.ng.js
@@ -6,11 +6,19 @@ angular.module('quick-survey').directive('addQuestionWithOptions', function () {
       existingOptions: '='
     },
     controller: function ($scope) {
+      $scope.otherAlreadyAdded = false;
+
       if (!$scope.existingOptions) {
         $scope.question.options = [{
           'value': '',
           'type': 'normal'
         }];
+      } else {
+        $scope.existingOptions.forEach(function (option) {
+          if (option.type === 'other') {
+            $scope.otherAlreadyAdded = true;
+          }
+        });
       }
 
       $scope.remove = function (idx) {
@@ -24,10 +32,22 @@ angular.module('quick-survey').directive('addQuestionWithOptions', function () {
         });
       };
 
+      $scope.removeOther = function () {
+        if ($scope.otherAlreadyAdded) {
+          $scope.otherAlreadyAdded = false;
+          $scope.question.options = $scope.question.options.filter(function (option) {
+            return option.type !== 'other';
+          });
+        }
+      };
+
       $scope.addOther = function () {
-        $scope.question.options.push({
-          'type': 'other'
-        });
+        if (!$scope.otherAlreadyAdded) {
+          $scope.otherAlreadyAdded = true;
+          $scope.question.options.push({
+            'type': 'other'
+          });
+        }
       };
     },
     templateUrl: 'client/js/manage/directives/add-question-with-options.ng.html',
